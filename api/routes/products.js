cde58a7ed1b074508b47b400db66abdf8e7f49b9
Afif Alfiano <m.afif@cakap.id>
2023-05-27 10:30:21 +0700

    use mongo for db

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,48 +1,80 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/products');
+const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        messsage: 'Handling GET request to /products'
+    Product.find().exec().then(doc => {
+        if (doc.length > 0) {
+            res.status(200).json({message: 'Success', data: [...doc]})
+        } else {
+            res.status(404).json({message: 'No entries data', data: []})
+        }
+    }).catch(err => {
+        res.status(500).json({error: err});
     })
 });
 
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price,
-    }
-    const language = req.headers['accept-language'];
-    console.log(language)
-    res.status(200).json({
-        messsage: 'Handling POST request to /products',
-        createdProduct: product,
-        language
+        price: req.body.price
     })
+    product.save().then(doc => {
+        console.log(doc);
+        res.status(201).json({message: 'Success', product: product._id})
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err})
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'You are success find the data',
-        id
-    })
+    Product.findById(id).exec().then(doc => {
+        console.log(doc)
+        if (doc) {
+        res.status(200).json(doc)
+        } else {
+            res.status(404).json({message: `No valid entry ID ${id}`})
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({error: err})
+    });
+
 })
 
 router.patch('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        messsage: 'Updated product!',
-        id
+    const updateOps = {};
+
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.findByIdAndUpdate({_id: id}, {$set: updateOps}).exec().then(result => {
+        if (result) {
+            res.status(200).json(result)
+        } else {
+            res.status(404).json({message: 'Data not valid'});
+        }
+    }).catch(err => {
+        res.status(500).json({error: err});
     })
 });
 
 router.delete('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    res.status(200).json({
-        messsage: 'Deleted product!',
-        id
+    Product.findByIdAndRemove({_id: id}).exec().then(doc => {
+        if (doc) {
+            res.status(200).json(doc)
+        } else {
+            res.status(404).json({message: `The entry data is not provided. ${id}`})
+        }
+    }).catch(err => {
+        res.status(500).json({error: err});
     })
 });
 
