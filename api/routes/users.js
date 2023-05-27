b188cd3171393545ed9f18ddd881f3e0ada2248c
Afif Alfiano <m.afif@cakap.id>
2023-05-27 19:09:23 +0700

    add jwt token for authorization

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next) => {
     User.find({email: req.body.email}).exec()
@@ -32,6 +33,34 @@ router.post('/signup', (req, res, next) => {
     })
     .catch(err => {
         res.status(500).json({error: err});
+    })
+});
+
+router.post('/signin', (req, res, next) => {
+    console.log(req)
+    User.find({email: req.body.email}).exec().then(user => {
+        if (user.length < 1) {
+            return res.status(401).json({message: 'Auth failed'});
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, response) => {
+            if (err) {
+                return res.status(401).json({message: 'Auth failed'}); 
+            }
+
+            if (response) {
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0].id,
+
+                }, process.env.JWT_KEY, {
+                    expiresIn: "1h",
+
+                })
+                return res.status(200).json({message: 'Auth successful', token});
+            }
+        })
+    }).catch(err => {
+        res.status(500).json({error: err})
     })
 });
 
